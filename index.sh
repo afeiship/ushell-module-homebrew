0ac8b1b97c375f5b3f4895b52285edd2c40b8f47
afeiship <1290657123@qq.com>
2018-05-07 09:10:51 +0800

    issue: brew install cmd

--- index.sh
+++ index.sh
@@ -3,6 +3,6 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 export HOMEBREW_BOTTLE_DOMAIN=http://7xkcej.dl1.z0.glb.clouddn.com
-export HOMEBREW_NO_AUTO_UPDATE=1
+alias brew-install="HOMEBREW_NO_AUTO_UPDATE=1 brew install";
 
 unset ROOT_PATH;
