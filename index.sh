3e063499daa420751e9ca29d00a4756a52e173f5
afeiship <1290657123@qq.com>
2018-05-10 19:34:15 +0800

    wip

--- index.sh
+++ index.sh
@@ -3,6 +3,10 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 export HOMEBREW_BOTTLE_DOMAIN=http://7xkcej.dl1.z0.glb.clouddn.com
+
 alias brew-install="HOMEBREW_NO_AUTO_UPDATE=1 brew install";
+alias bs-start="brew services start ";
+alias bs-stop="brew services stop ";
+alias bs-list="brew services list ";
 
 unset ROOT_PATH;
