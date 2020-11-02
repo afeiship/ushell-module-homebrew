c147b68993cf12a2dff609cebc71ba42c0a81c15
afeiship <1290657123@qq.com>
2020-11-03 07:20:48 +0800

    feat: update alias

--- index.sh
+++ index.sh
@@ -2,10 +2,12 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-alias brew-install="HOMEBREW_NO_AUTO_UPDATE=1 brew install";
-alias bs-start="brew services start ";
-alias bs-stop="brew services stop ";
-alias bs-list="brew services list ";
+alias bws-install="HOMEBREW_NO_AUTO_UPDATE=1 brew install";
+alias bws-list=brew services list
+alias bws-start=brew services start
+alias bws-stop=brew services stop
+alias bws-restart=brew services restart
+alias bws-cleanup=brew services cleanup
 
 # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles;
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles;
