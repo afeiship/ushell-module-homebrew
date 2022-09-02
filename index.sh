5717df664797e9bc853d25fddfb0fd0ee0050374
afeiship <1290657123@qq.com>
2022-09-02 20:23:36 +0800

    chore: add nir

--- index.sh
+++ index.sh
@@ -2,12 +2,17 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
+# brew alias
 alias bw-install="HOMEBREW_NO_AUTO_UPDATE=1 brew install";
-alias bws-list="brew services list"
-alias bws-start="brew services start"
-alias bws-stop="brew services stop"
-alias bws-restart="brew services restart"
-alias bws-cleanup="brew services cleanup"
+alias bw-clean="cd /usr/local/Homebrew/Library/Taps/afeiship && rm -rf *";
+
+# brew services
+alias bws-list="brew services list";
+alias bws-start="brew services start";
+alias bws-stop="brew services stop";
+alias bws-restart="brew services restart";
+alias bws-cleanup="brew services cleanup";
+
 
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles;
 # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles;
